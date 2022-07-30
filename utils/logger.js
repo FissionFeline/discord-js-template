e41c9ecf12cf140eb35d1151170cd39b319356c1
i-spin <me@i-sp.in>
2022-07-29 23:03:22 -0700

    use logger

---
+++ utils/logger.js
@@ -0,0 +1,22 @@
+const colors = require('colors/safe');
+
+class Logger {
+  static info(message) {
+    console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan('[INFO]')} ${message}`);
+  }
+
+  static success(message) {
+    console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green('[SUCCESS]')} ${message}`);
+  }
+
+  static warn(message) {
+    fs.appendFileSync(this.logName, `${(new Date()).toLocaleString()} [WARN] ${message}\n`);
+    console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow('[WARN]')} ${message}`);
+  }
+
+  static error(message) {
+    console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red('[ERROR]')} ${message}`);
+  }
+}
+
+module.exports = Logger;
