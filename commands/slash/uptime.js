64ff3db6af085086f780aacd25e830a40d747d42
i-spin <me@i-sp.in>
2022-07-30 17:09:38 -0700

    lint and format command files

--- commands/slash/uptime.js
+++ commands/slash/uptime.js
@@ -1,20 +1,20 @@
+const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    name: "uptime",
-    description: "displays the bots current uptime",
-    register_command: new SlashCommandBuilder()
-        .setName('uptime')
-        .setDescription('Displays bot uptime!'),
-    execute(client, interaction) {
-        const { MessageEmbed } = require('discord.js');
-        const uptime_embed = new MessageEmbed()
-            .setColor('#dac188')
-            .setTitle(`${client.user.username} uptime!`)
-            .setAuthor({ name: interaction.user.tag + ' ', iconURL: interaction.user.avatarURL() })
-            .setDescription(`This is the current uptime of ${client.user.username}!\n**${client.uptime / 1000 / 60} Minutes!**`)
-            .setTimestamp()
+  name: 'uptime',
+  description: 'displays the bots current uptime',
+  register_command: new SlashCommandBuilder()
+    .setName('uptime')
+    .setDescription('Displays bot uptime!'),
+  execute(client, interaction) {
+    const uptimeEmbed = new MessageEmbed()
+      .setColor('#dac188')
+      .setTitle(`${client.user.username} uptime!`)
+      .setAuthor({ name: `${interaction.user.tag} `, iconURL: interaction.user.avatarURL() })
+      .setDescription(`This is the current uptime of ${client.user.username}!\n**${client.uptime / 1000 / 60} Minutes!**`)
+      .setTimestamp();
 
-        interaction.reply({ embeds: [uptime_embed] })
-    }
-}
+    interaction.reply({ embeds: [uptimeEmbed] });
+  },
+};
--- commands/text/here.js
+++ commands/text/here.js
@@ -1,11 +1,10 @@
 module.exports = {
-    name: "here",
-    description: "Creates a new embed",
-    usage: '>embed_create "titel" "Description"',
-    trigger: "here",
-    category: "Stuff",
-    async execute(client, msg, args) {
-
-        msg.channel.send()
-    }
-}
+  name: 'here',
+  description: 'Creates a new embed',
+  usage: '>embed_create "titel" "Description"',
+  trigger: 'here',
+  category: 'Stuff',
+  async execute(_client, msg, _args) {
+    msg.channel.send('hello :D');
+  },
+};
