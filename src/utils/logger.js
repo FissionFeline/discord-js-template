5875a749c5ab74a4c4e2b82659d8652b2b372bed
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-05-31 00:55:36 +0200

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,20 +1,23 @@
 const colors = require('colors/safe');
 
 class Logger {
-    static info(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan('[INFO]')} ${message}`);
+    constructor(origin) {
+        this.origin = origin
+    }
+    info(message) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[INFO] [${origin}]`)} ${message}`);
     }
 
-    static success(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green('[SUCCESS]')} ${message}`);
+    success(message) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[SUCCESS] [${origin}]`)} ${message}`);
     }
 
-    static warn(message) {
-        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow('[WARN]')} ${message}`);
+    warn(message) {
+        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[WARN] [${origin}]`)} ${message}`);
     }
 
-    static error(message) {
-        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red('[ERROR]')} ${message}`);
+    error(message) {
+        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[ERROR] [${origin}]`)} ${message}`);
     }
 }
 
