9b326e8d3ecc78e1ffc3f53a3877134887483fd0
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-05-31 01:01:37 +0200

    implemented logger

--- src/utils/initialisation/find_events.js
+++ src/utils/initialisation/find_events.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
-const logger = new require('../logger.js')
+const log = new require('../logger.js')
+const logger = new log("Event loader")
 const path_to_events = __dirname + '/../../events/'
 
 module.exports = (client) => {
@@ -7,21 +8,23 @@ module.exports = (client) => {
 
     for (const element of events) {
         const element_loaded = require(path_to_events + element)
-        if (!element_loaded.type) return console.log(`Failed to load ${element} type`)
-        if (!element_loaded.call) return console.log(`Failed to load ${element} call`)
+        if (!element_loaded.type) return logger.error(`Failed to load ${element} type`)
+        if (!element_loaded.call) return logger.error(`Failed to load ${element} call`)
         switch (element_loaded.type) {
             case "on":
                 {
+                    logger.success(`Successfully loaded ${element} event`)
                     client.on(element_loaded.event, (...args) => element_loaded.call(...args));
                     break;
                 }
             case "once":
                 {
+                    logger.success(`Successfully loaded ${element} event`)
                     client.once(element_loaded.event, (...args) => element_loaded.call(...args));
                     break;
                 }
             default:
-                return console.log(`Type of ${element} is not allowed!`)
+                return logger.error(`Type of ${element} is not allowed!`)
         }
     }
 
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,19 +5,19 @@ class Logger {
         this.origin = origin
     }
     info(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[INFO] [${origin}]`)} ${message}`);
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[INFO] [${this.origin}]`)} ${message}`);
     }
 
     success(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[SUCCESS] [${origin}]`)} ${message}`);
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[SUCCESS] [${this.origin}]`)} ${message}`);
     }
 
     warn(message) {
-        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[WARN] [${origin}]`)} ${message}`);
+        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[WARN] [${this.origin}]`)} ${message}`);
     }
 
     error(message) {
-        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[ERROR] [${origin}]`)} ${message}`);
+        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[ERROR] [${this.origin}]`)} ${message}`);
     }
 }
 
