88e0d2e55cc61db00a5ef7fa904aa050e1f8834c
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-06-12 00:31:34 +0200

    wadawf

--- src/utils/initialisation/register_commands.js
+++ src/utils/initialisation/register_commands.js
@@ -1,18 +1,21 @@
 const { REST, Routes } = require('discord.js');
+const log = new require('../logger.js')
+const logger = new log("Command register")
 
 module.exports = async (client, commands) => {
+    if(!commands || commands.length == 0) return logger.info("No commands to register")
     const rest = new REST().setToken(process.env.TOKEN);
 
     try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        logger.info(`Registering ${commands.length} command${commands.length > 1?"s":""} to the discord api ...`)
 
         const data = await rest.put(
             Routes.applicationCommands(client.user.id),
             { body: commands },
         );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        logger.success(`Successfully registered ${data.length} application (/) command${data.length > 1?"s":""}.`);
     } catch (error) {
-        console.error(error);
+        logger.error(error)
     }
 }
