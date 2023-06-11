afbf9627dace94635d1303879706d271f9e0a5f9
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-06-11 23:52:01 +0200

    Update find_commands.js

--- src/utils/initialisation/find_commands.js
+++ src/utils/initialisation/find_commands.js
@@ -1,12 +1,17 @@
 const fs = require('fs')
 const log = new require('../logger.js')
 const logger = new log("Command loader")
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const path_to_commands = __dirname + '/../../commands/'
 
 module.exports = (client) => {
     const commands = fs.readdirSync(path_to_commands).filter(file => file.endsWith('.js'));
 
     for (const command of commands) {
+        const command_loaded = require(command)
+        if(!command_loaded) return logger.error(`${command} not valid`)
+        if(command_loaded.execute !== 'function') return logger.error(`${command} does not have the execute function`)
+        if(command_loaded.register_command instanceof SlashCommandBuilder == false) return logger.error(`${command} does not have the register_command SlashcommandBuilder instance`)
         
     }
 
