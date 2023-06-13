f6f46becbba01fc0bd4ec1b471ee8f38ead3f343
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-06-13 02:29:23 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js")
-
+const log = new require('../utils/logger.js')
+const logger = new log("ready") 
 module.exports = {
     event: Discord.Events.ClientReady,
     type: "once",
     async call(client) {
-        console.log(`Connected ! You're in as ${client.user.username} currently serving ${client.guilds.cache.size} Server(s)`)
+        logger.success(`Connected ! You're in as ${client.user.username} currently serving ${client.guilds.cache.size} Server(s)`)
     }
 }
