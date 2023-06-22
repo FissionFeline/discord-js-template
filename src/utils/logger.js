8859ff8a1e2421821ff17c2545b08f27a42368d1
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2023-06-22 02:18:38 +0200

    I think this looks nicer
    This looks cooler

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,19 +5,19 @@ class Logger {
         this.origin = origin
     }
     info(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[INFO] [${this.origin}]`)} ${message}`);
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[${this.origin}] [INFO]`)} ${message}`);
     }
 
     success(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[SUCCESS] [${this.origin}]`)} ${message}`);
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[${this.origin}] [SUCCESS]`)} ${message}`);
     }
 
     warn(message) {
-        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[WARN] [${this.origin}]`)} ${message}`);
+        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[${this.origin}] [WARN]`)} ${message}`);
     }
 
     error(message) {
-        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[ERROR] [${this.origin}]`)} ${message}`);
+        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[${this.origin}] [ERROR]`)} ${message}`);
     }
 }
 
